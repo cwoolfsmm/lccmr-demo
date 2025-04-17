9e6bf49fa97311e101f4b5ba1d015be666764b8a
Cici Woolf <cwoolf@mac.mynetworksettings.com>
2025-04-17 11:20:21 -0500

    Setup for tailwind, basic homepage

---
+++ src/app/data.js
@@ -0,0 +1,12 @@
+export const data = {
+  homePage: {
+    title: "Minnesota: Our Environment. Our Stories.",
+    cardTitle: "Northern Lakes and Forests",
+    cardSubtitle: "A Drop in the Lakes",
+    cardContent: "The Legislative-Citizen Commission on Minnesota Resources (LCCMR) is a bipartisan commission of the Minnesota Legislature that makes funding recommendations for the Environment and Natural Resources Trust Fund (ENRTF). The LCCMR was established in 1967 to ensure that the ENRTF is used to protect, conserve, and enhance Minnesota's environment and natural resources.",
+    cardMedia: "",
+  }
+}
+
+
+
--- src/app/page.js
+++ src/app/page.js
@@ -1,57 +1,37 @@
 import Image from "next/image";
+import { data } from "./data";
+
+//GraphQL query will go here
 
 export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm/6 text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-[family-name:var(--font-geist-mono)] font-semibold">
-              src/app/page.js
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
+  const { homePage } = data;
+  const { title, cardTitle, cardSubtitle, cardContent, cardMedia } = homePage;
+  return (
+    // flex and flex-col props to set flex direction to column
+    <div className="min-h-screen flex flex-col p-8">
+      <header> 
+        <h1 className="text-[64px]/12 font-black">{title}</h1>
+        <hr className="text-(--bright-blue) border-2"></hr>
+      </header>
+      <main className="flex items-center justify-center mt-8">
+       <section>
+        <div>
+          <h2 className="text-[40px]/12 text-(--dark-gold) font-black mb-2">{cardTitle}</h2>
+          <h3 className="text-xl text-(--dark-green) font-bold mb-2">{cardSubtitle}</h3>
+          <p className="text-sm text-(--dark-green)">{cardContent}</p>
+          {cardMedia && (
             <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
+              src={cardMedia}
+              alt={cardTitle}
+              width={500}
+              height={300}
             />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+          )}
         </div>
+       </section>
       </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
+      <footer className="mt-auto flex gap-6 flex-wrap items-center justify-center">
         <a
           className="flex items-center gap-2 hover:underline hover:underline-offset-4"
           href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
